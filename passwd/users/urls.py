1d0620fb6c7ad0bc88249e7a395602849192faa2
Hunter Thompson <thompson.grey.hunter@gmail.com>
2019-05-26 09:23:24 +0000

    Removed database related modules and information

--- passwd/users/urls.py
+++ passwd/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views 
 
 urlpatterns = [
-    path('$', views.index, name='index'),
+    path('', views.index, name='index'),
     path('query', views.query, name='query'),
     re_path(r'^[0-9]+$', views.uid, name='uid'),
     re_path(r'^[0-9]+/groups$', views.group_uid, name='user_groups')
